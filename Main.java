342924c9ea14d0cd9aeccadef637cea956c53514
Bhavya Tanugula <bhavya.tanugula@gmail.com>
2023-12-07 12:00:24 -0500

    Implemented Lamda Expression

--- Main.java
+++ Main.java
@@ -2,7 +2,20 @@ public class Main{
 
     public static void main(String[]args){
 
-        
+        NumAdder MyAdder = (int a, int b) -> {
+
+            int sum = 0; 
+            
+            for(int i=a; i<=b; i++){
+                sum+=i; 
+            }
+
+
+            return sum; 
+
+        }; 
+
+        int result = MyAdder.add(1,2); 
 
 
     }
